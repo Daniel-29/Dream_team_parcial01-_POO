db3b74145e2e7cbe4e170efb5207e299f85f1233
Javier Villatoro <javier99.villatoro@gmail.com>
2020-04-17 15:06:31 -0600

    Add Class CalculadoraImpuesto, Documento, Empresa

---
+++ src/com/JAVJ/x00199919/CalculadoraImpuesto.java
@@ -0,0 +1,38 @@
+package com.JAVJ.x00199919;
+
+public final class CalculadorarImpuestos {
+    private static Double totalRenta = 0.0, totalISSS = 0.0 , totalAFP = 0.0;
+
+    private CalculadorarImpuestos() {
+    }
+    public double CalcularPago(Empleado empleado){
+        double pago = 0,salario=empleado.getSalario();
+        if(empleado instanceof ServicioProfesional){
+            totalRenta =salario*0.1;
+            pago= salario- totalRenta;
+        }else if(empleado instanceof PlazaFija){
+            totalISSS = salario*totalISSS;
+            totalAFP = salario*totalAFP;
+            double restante =  salario - totalISSS - totalAFP;
+            if(restante <= 472 && restante >=0.01){
+                totalRenta=0.0;
+            }else if(restante <= 895.24 && restante >=472.01){
+                totalRenta = 0.1*(restante-472)+17.67;
+            }else if(restante <= 2038.10 && restante >=895.25){
+                totalRenta = 0.2*(restante-895.24)+60;
+            }else if(restante >=2038.11){
+                totalRenta = 0.3*(restante-2038.10)+288.57;
+            }
+            pago = restante - totalRenta;
+        }
+     return pago;
+    }
+    public String MostrarTotales(){
+        return "Descuentos{" +
+                " Renta:" + totalRenta +"$"+
+                ", ISSS:" + totalISSS +"$"+
+                ", AFP:=" + totalAFP +"$"+
+                '}';
+    }
+
+}
---
+++ src/com/JAVJ/x00199919/Empresa.java
@@ -0,0 +1,28 @@
+package com.JAVJ.x00199919;
+
+import java.util.ArrayList;
+
+public class Empresa {
+    private  String nombre;
+    private ArrayList<Empleado> empleados;
+
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        empleados = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void addEmpleados(Empleado empleado) {
+       empleados.add(empleado);
+    }
+    public void quitEmpleados(String nombre) {
+        empleados.removeIf(e -> (e.getNombre().equals(nombre)));
+    }
+}
